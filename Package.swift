eb3561f84bffc89ab5eb99b1383b93a57229c421
1amageek <tmy0x3@icloud.com>
2026-02-14 10:38:57 +0900

    Switch fdb-swift-bindings to local path for lifecycle changes

--- Package.swift
+++ Package.swift
@@ -32,8 +32,7 @@ let package = Package(
         .package(url: "https://github.com/1amageek/database-kit.git", from: "26.0213.0"),
         .package(url: "https://github.com/1amageek/swift-hnsw.git", from: "0.2.1"),
         .package(
-            url: "https://github.com/1amageek/fdb-swift-bindings.git",
-            branch: "feature/directory-layer"
+            path: "../fdb-swift-bindings"
         ),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.7.0"),
