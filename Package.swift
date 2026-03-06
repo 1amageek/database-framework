8f3a4cafbb188a89fcac4b231319fe4f1f2b2a74
1amageek <tmy0x3@icloud.com>
2026-03-06 16:30:40 +0900

    Change storage-kit dependency from local path to GitHub URL

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/1amageek/database-kit.git", from: "26.0222.1"),
         .package(url: "https://github.com/1amageek/swift-hnsw.git", from: "0.2.1"),
-        .package(path: "../storage-kit"),
+        .package(url: "https://github.com/1amageek/storage-kit.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.7.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "4.2.0"),
