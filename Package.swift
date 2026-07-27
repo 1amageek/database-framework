52b56be709d8fc762acfbe17c144087a555915c6
1amageek <tmy0x3@icloud.com>
2026-07-27 10:02:26 +0900

    Resolve vector index from SwiftHNSW 1.0.0

--- Package.swift
+++ Package.swift
@@ -56,7 +56,10 @@ let package = Package(
             url: "https://github.com/1amageek/database-kit.git",
             from: "26.0727.1"
         ),
-        .package(path: "../swift-hnsw"),
+        .package(
+            url: "https://github.com/1amageek/swift-hnsw.git",
+            from: "1.0.0"
+        ),
         .package(path: "../storage-kit"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.7.0"),
