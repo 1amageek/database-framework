0cdafff847eb307e8827df08ccaacf38aa5e6bb1
1amageek <tmy0x3@icloud.com>
2026-08-03 04:19:28 +0900

    Resolve released database dependencies

--- Package.swift
+++ Package.swift
@@ -107,7 +107,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/1amageek/database-kit.git",
-            from: "26.0731.0"
+            from: "26.0803.0"
         ),
         .package(
             url: "https://github.com/1amageek/swift-hnsw.git",
@@ -115,7 +115,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/1amageek/storage-kit.git",
-            from: "26.0731.0"
+            from: "26.0803.0"
         ),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", from: "2.7.0"),
